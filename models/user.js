24d288691ef06dba9b4abebea12931b75b3f7121
Stanislava Filonova <stanislavafilonova@yandex.ru>
2022-05-12 13:12:41 +0300

    fix tests 7

--- models/user.js
+++ models/user.js
@@ -6,19 +6,16 @@ const userSchema = new mongoose.Schema({
     required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
     minlength: 2, // минимальная длина имени — 2 символа
     maxlength: 30, // а максимальная — 30 символов
-    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
     required: true,
-    default: 'Исследователь',
   },
   avatar: {
     type: String,
     required: true,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
